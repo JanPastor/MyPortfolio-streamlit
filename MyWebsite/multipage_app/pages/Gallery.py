073ceca0b1ea3684f5881fce2a175beed94a2ef7
Jan Pastor <110146556+JanPastor@users.noreply.github.com>
2024-01-04 20:47:18 +0000

    Updated the functions in Gallery file

--- MyWebsite/multipage_app/pages/Gallery.py
+++ MyWebsite/multipage_app/pages/Gallery.py
@@ -14,75 +14,71 @@ import os
 
 # ChatGPT suggested function to load specific images into a dictionary
 def load_specific_images(image_data):
-    images = {}
     for name, (url, caption) in image_data.items():
         st.image(url, caption=caption, use_column_width=True)
-    return images 
 
 
 # Function to load specific videos into a dictionary
 def load_specific_videos(video_data):
-    videos = {}
     for name, (url, caption) in video_data.items():
         st.video(url, caption=caption, use_column_width=True)
-    return videos
 
 
 # Dictionary of image paths
 image_data = { # This is the first dictionary set of pictures
-    "ROV_Competition": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/MATE_ROV.pngraw=true", "🌊 ROV Competition 2023 🌊"),
-    "Soldering_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_soldering_1.jpgraw=true", "Top: Jan Pastor is soldering; Bottom: Brycen Lee researching"),
-    "Soldering_2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Rene_n_me_soldering.jpegraw=true", "Left: Me soldering with Rene"),
-    "Soldering_3": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_soldering_2.jpgraw=true", "Close up: Me soldering Surface Mount Resistor.😧"),
-    "Soldering_4": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_soldering_tether.jpegraw=true","Soldering the Power tether 🔥"),
-    "Mentoring_solder": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Mentoring_soldering.jpgraw=true", "Mentoring Jasmine Lai on the Art of Soldering 🙃"),
-    "Supervising": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_supervising.jpgraw=true","I'm supervising my protoge, Brycen Lee 😝"),
+    "ROV_Competition": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/MATE_ROV.png?raw=true", "🌊 ROV Competition 2023 🌊"),
+    "Soldering_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_soldering_1.jpg?raw=true", "Top: Jan Pastor is soldering; Bottom: Brycen Lee researching"),
+    "Soldering_2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Rene_n_me_soldering.jpeg?raw=true", "Left: Me soldering with Rene"),
+    "Soldering_3": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_soldering_2.jpg?raw=true", "Close up: Me soldering Surface Mount Resistor.😧"),
+    "Soldering_4": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_soldering_tether.jpeg?raw=true","Soldering the Power tether 🔥"),
+    "Mentoring_solder": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Mentoring_soldering.jpg?raw=true", "Mentoring Jasmine Lai on the Art of Soldering 🙃"),
+    "Supervising": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_supervising.jpg?raw=true","I'm supervising my protoge, Brycen Lee 😝"),
 }
 image_data_1 = { # This is the second dictionary set of pictures]
-    "Brainstorming": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Software_flowchart.jpgraw=true", "Designing software heirarchy with my assistant DaNa 🤔"),
-    "Software_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_explain_code_1.jpegraw=true","Me setting up thruster control code demo 😃"),
-    "Software_2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_explain_code_2.jpgraw=true", "Team is gathered. Explanation begins. 😬"),
-    "Software_3": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_explain_code_5.JPGraw=true", "Further explaining the code, with assistant DaNa👍"),
-    "Software_4": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_Explaining_code_favorite.JPGraw=true", "The epitome of mentorship 🤓"),
-    "Software_5": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_explain_to_Adam.jpegraw=true","Explaining code to mentee Adam"),
-    "Software_6": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Testing_cameras.jpgraw=true","Debugging ROV Camera System 😓")
+    "Brainstorming": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Software_flowchart.jpg?raw=true", "Designing software heirarchy with my assistant DaNa 🤔"),
+    "Software_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_explain_code_1.jpeg?raw=true","Me setting up thruster control code demo 😃"),
+    "Software_2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_explain_code_2.jpg?raw=true", "Team is gathered. Explanation begins. 😬"),
+    "Software_3": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_explain_code_5.JPG?raw=true", "Further explaining the code, with assistant DaNa👍"),
+    "Software_4": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_Explaining_code_favorite.JPG?raw=true", "The epitome of mentorship 🤓"),
+    "Software_5": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_explain_to_Adam.jpeg?raw=true","Explaining code to mentee Adam"),
+    "Software_6": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Testing_cameras.jpg?raw=true","Debugging ROV Camera System 😓")
 }
 image_data_2 = { # This is the third dictionary set of pictures about ROV, Team photos, and other Lancer Lumineer stuff
-    "ChessDowntime": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_Chess.jpgraw=true", "Phew! Chess downtime 😌"),
-    "TeamLeads": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/TeamLeadsPrior.jpgraw=true","Team Leads!"),
-    "GUI_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_GUI.jpgraw=true","The fruits of my labor 😭"),
-    "ROV_dry_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_front_center.jpgraw=true", "Front view"),
-    "ROV_dry_2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_rear.jpgraw=true","Rear view"),
-    "SystemCheck":("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/System_check1.jpgraw=true","System check"),
-    "Pool_test_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/pool_test_1.jpgraw=true", "Poolside test w/ ROV pilot")
+    "ChessDowntime": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Me_Chess.jpg?raw=true", "Phew! Chess downtime 😌"),
+    "TeamLeads": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/TeamLeadsPrior.jpg?raw=true","Team Leads!"),
+    "GUI_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_GUI.jpg?raw=true","The fruits of my labor 😭"),
+    "ROV_dry_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_front_center.jpg?raw=true", "Front view"),
+    "ROV_dry_2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_rear.jpg?raw=true","Rear view"),
+    "SystemCheck":("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/System_check1.jpg?raw=true","System check"),
+    "Pool_test_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/pool_test_1.jpg?raw=true", "Poolside test w/ ROV pilot")
 }
 image_data_3 = { # Continuing image data set 2 but using a new container for aesthetic appeal
-    "Team Photo":("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Team_Photo.jpgraw=true","Team Photo 🤪"),
-    "ROV_wet_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_water1.jpgraw=true", "Demo 1 🤖"),
-    "ROV_wet_2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_water2.jpgraw=true", "Demo 2 🤖"),
-    "Competition": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Competition.jpgraw=true", "Competition time! 💪"),
-    "Main deck crew": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Team_Leads.jpgraw=true","Deck crew assembled! 🙌"),
-    "Station_0": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/at_the_deck.jpgraw=true","Station 0"),
-    "Copilot_Pilot": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Mission1_me_and_DaNa.jpgraw=true","Copilot: Jan Pastor ; Pilot: DaNa")
+    "Team Photo":("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Team_Photo.jpg?raw=true","Team Photo 🤪"),
+    "ROV_wet_1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_water1.jpg?raw=true", "Demo 1 🤖"),
+    "ROV_wet_2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/ROV_water2.jpg?raw=true", "Demo 2 🤖"),
+    "Competition": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Competition.jpg?raw=true", "Competition time! 💪"),
+    "Main deck crew": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Team_Leads.jpg?raw=true","Deck crew assembled! 🙌"),
+    "Station_0": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/at_the_deck.jpg?raw=true","Station 0"),
+    "Copilot_Pilot": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Spring2023/Mission1_me_and_DaNa.jpg?raw=true","Copilot: Jan Pastor ; Pilot: DaNa")
 }
 image_data_4 = { # Pictures from MATE ROV FLOATS Summer Camp 2023 PART I
-    "first_day1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/First_day.jpgraw=true","First day of class"),
-    "first_day_puget": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/First_day2.jpgraw=true", "Model of Puget Sound Estuary"),
-    "Code_sensor": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Coding_sensor.jpgraw=true","Me coding, while Jasmine checks instructions. Teamwork! 😤 💪"),
-    "Build_circuit": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Build_sensor.jpgraw=true", "Assembling the sensor 😐"),
-    "Deploy_sensor": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Jasmine_deploy_sensor.jpgraw=true", "Jasmine deploying our completed sensor 👏"),
-    "Deploy_sensor2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/deploy_sensors2.jpgraw=true","Installing more sensors📡"),
-    "Sensor_array": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Sensor_array.jpgraw=true","Sensor array nearly complete 🤖")
+    "first_day1": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/First_day.jpg?raw=true","First day of class"),
+    "first_day_puget": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/First_day2.jpg?raw=true", "Model of Puget Sound Estuary"),
+    "Code_sensor": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Coding_sensor.jpg?raw=true","Me coding, while Jasmine checks instructions. Teamwork! 😤 💪"),
+    "Build_circuit": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Build_sensor.jpg?raw=true", "Assembling the sensor 😐"),
+    "Deploy_sensor": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Jasmine_deploy_sensor.jpg?raw=true", "Jasmine deploying our completed sensor 👏"),
+    "Deploy_sensor2": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/deploy_sensors2.jpg?raw=true","Installing more sensors📡"),
+    "Sensor_array": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Sensor_array.jpg?raw=true","Sensor array nearly complete 🤖")
 }
 
 image_data_5 = { # Pictures from MATE ROV FLOATS Summer Camp 2023 PART II
-    "Tour": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Tour1.jpgraw=true","Tour of Ocean Tech Building 🤓"),
-    "Float_Inspection": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Me_float_inspecting.jpgraw=true", "Examining FLOAT electronics 🔬"),
-    "In_front_float": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/In_front_FLOAT.jpgraw=true"," 😁"),
-    "Glider": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Glider.jpgraw=true", "The Glider 🌊"),
-    "Lab_work": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Lab_training.jpgraw=true", " Lab training 🥼"),
-    "Aft_deck_photo": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/aft_deck_group_pic.jpgraw=true","👋"),
-    "Rossetta_device": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/ROSSETTA.jpgraw=true","Meet Rossetta 🤯")
+    "Tour": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Tour1.jpg?raw=true","Tour of Ocean Tech Building 🤓"),
+    "Float_Inspection": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Me_float_inspecting.jpg?raw=true", "Examining FLOAT electronics 🔬"),
+    "In_front_float": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/In_front_FLOAT.jpg?raw=true"," 😁"),
+    "Glider": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Glider.jpg?raw=true", "The Glider 🌊"),
+    "Lab_work": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/Lab_training.jpg?raw=true", " Lab training 🥼"),
+    "Aft_deck_photo": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/aft_deck_group_pic.jpg?raw=true","👋"),
+    "Rossetta_device": ("https://github.com/JanPastor/my-streamlit-staticmedia/blob/main/MATE_Floats/ROSSETTA.jpg?raw=true","Meet Rossetta 🤯")
 }
 
 # Dictionary of video paths and captions
