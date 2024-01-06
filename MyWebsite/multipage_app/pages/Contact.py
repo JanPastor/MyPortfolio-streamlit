645f0a1a14a3c790b51705cef514b1dae5840071
Jan Pastor <110146556+JanPastor@users.noreply.github.com>
2024-01-06 23:46:16 +0000

    Updates to the contact form

--- MyWebsite/multipage_app/pages/Contact.py
+++ MyWebsite/multipage_app/pages/Contact.py
@@ -1,43 +1,5 @@
 import streamlit as st
 
-# # Use Local CSS 
-# def local_css(file_name):
-#     with open(file_name) as f:
-#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html= True)
-
-# local_css("MyWebsite/style/style.css") # path to local CSS file
-
-
-# def show():
-    
-#     st.markdown("""
-#     <style>
-#         #MainMenu {visibility: visible;}
-#         footer {visibility: hidden;}
-#         header {visibility: visible;}
-#     </style>
-#     """, unsafe_allow_html=True)
-
-
-#     with st.container():
-#         st.title("Contact :email:")
-#         st.write("Contact me! :cat:")
-#         st.divider()
-#         #  st.write("##") # Extra padding
-
-#     contact_form = """
-#     <form action="https://formsubmit.co/janpastor123@gmail.com" method="POST">
-#     <input type = "hidden" name = "captcha" value = "True">
-#     <input type="text" name="name" placeholder="Your name" required>
-#     <input type="email" name="email" placeholder = "Your email" required>
-#     <textarea name = "message" placeholder = "Your message here" required></textarea>
-#     <button type="submit" >Send</button>
-# </form>
-# """
-#     st.markdown(contact_form, unsafe_allow_html= True)
-
-import streamlit as st
-
 def show():
     st.markdown("""
     <style>
